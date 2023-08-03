de0dc85f28526878ba98d603d87cca8f81bea18e
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2023-08-03 22:44:58 +0100

    component

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -4,7 +4,6 @@ import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
 import Notifications from "../Notifications/Notifications";
-import "./App.css";
 import PropTypes from "prop-types";
 import { getLatestNotification } from "../utils/utils";
 
